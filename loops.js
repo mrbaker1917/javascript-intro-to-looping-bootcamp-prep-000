7d58683393575ab030cfc8df56b14a7e5f076786
Mark Baker <mrbaker1917@gmail.com>
2018-07-19 02:21:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,7 +20,7 @@ function maybeTrue() {
   return Math.random() >= 0.5;
 }
 
-function doWhhileLoop(array) {
+function doWhileLoop(array) {
   do {
     array.pop();
   } while (array.length > 0 || maybeTrue());
